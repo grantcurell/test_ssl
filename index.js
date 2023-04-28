90085061c2ab00c05a926f402f8039eff3424dec
Grant Curell <grantcurell@gmail.com>
2023-04-28 11:22:07 -0400

    Simplified

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 // Node.js program to demonstrate the
 // tlsSocket.getPeerCertificate() method
 
+const util = require('util');
+
 const tls = require('tls'),
 	fs = require('fs'),
 
 	// Port and host address for server
 	PORT = 1337,
-	HOST = '127.0.0.1',
-	value = null;
+	HOST = '127.0.0.1';
 
 // Private key and public certificate for access
 const options = {
-	key: fs.readFileSync('private-key.pem'),
-	cert: fs.readFileSync('public-cert.pem'),
-	rejectUnauthorized: false
+	key: fs.readFileSync('./server_certs/grant.dell.com.key'),
+	cert: fs.readFileSync('./server_certs/grant.dell.com.crt'),
+	requestCert: true,
+	ca: [fs.readFileSync('./server_certs/rootCA.crt')],
+	rejectUnauthorized: true
 };
 
 // Creating and initializing server
 const server = tls.createServer(options, function (socket) {
+	// do nothing here
+});
 
-	// Print the data that we received
-	socket.on('data', function (data) {
-		console.log('\nReceived: %s ',
-			data.toString().replace(/(\n)/gm, ""));
-	});
+server.on('secureConnection', function (socket) {
+	// Getting peer certificate
+	// by using tlsSocket.getPeerCertificate() method
+	const value = socket.getPeerCertificate(true);
+	//value['valid_from'] = value['valid_from'].toString();
+	socket.write("peer certificate : " + util.inspect(value));
 
 	// Stopping the server
 	// by using the close() method
 	server.close(() => {
-		console.log("Server closed successfully");
+		console.log("Server closed successfully")
 	});
 });
 
@@ -40,14 +46,16 @@ server.listen(PORT, HOST, function () {
 
 // Creating and initializing client
 const client = tls.connect(PORT, HOST, options, function () {
+	console.log("client is connected");
+});
 
-	// Getting peer certificate
-	// by using tlsSocket.getPeerCertificate() method
-	let value = client.getPeerCertificate(false);
+client.on("data", function (data) {
 
-	client.write("peer certificate : " + value.serialNumber);
+	console.log('Received: %s',
+		data.toString().replace(/(\n)/gm, ""));
 
+	// Close the connection after receiving the message
 	client.end(() => {
-		console.log("Client closed successfully");
+		console.log("client closed successfully")
 	});
 });
