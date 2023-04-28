d3ca9f05f14b162a7c58a058f2557cb2ceabea5c
Grant Curell <grantcurell@gmail.com>
2023-04-28 09:14:15 -0400

    works

--- index.js
+++ index.js
@@ -6,27 +6,29 @@ const tls = require('tls'),
 
 	// Port and host address for server
 	PORT = 1337,
-	HOST = '127.0.0.1';
+	HOST = '127.0.0.1',
+	value = null;
 
 // Private key and public certificate for access
 const options = {
-  key: fs.readFileSync('./server_certs/server.key'),
-  cert: fs.readFileSync('./server_certs/server.crt'),
+	key: fs.readFileSync('private-key.pem'),
+	cert: fs.readFileSync('public-cert.pem'),
 	rejectUnauthorized: false
 };
 
 // Creating and initializing server
 const server = tls.createServer(options, function (socket) {
 
-	// Getting peer certificate
-	// by using tlsSocket.getPeerCertificate() method
-	value = socket.getPeerCertificate(true);
-	socket.write("peer certificate : " + value);
+	// Print the data that we received
+	socket.on('data', function (data) {
+		console.log('\nReceived: %s ',
+			data.toString().replace(/(\n)/gm, ""));
+	});
 
 	// Stopping the server
 	// by using the close() method
 	server.close(() => {
-		console.log("Server closed successfully")
+		console.log("Server closed successfully");
 	});
 });
 
@@ -38,16 +40,14 @@ server.listen(PORT, HOST, function () {
 
 // Creating and initializing client
 const client = tls.connect(PORT, HOST, options, function () {
-	console.log("client is connected");
-});
 
-client.on("data", function (data) {
+	// Getting peer certificate
+	// by using tlsSocket.getPeerCertificate() method
+	let value = client.getPeerCertificate(false);
 
-	console.log('Received: %s',
-		data.toString().replace(/(\n)/gm, ""));
+	client.write("peer certificate : " + value.serialNumber);
 
-	// Close the connection after receiving the message
 	client.end(() => {
-		console.log("client closed successfully")
+		console.log("Client closed successfully");
 	});
 });
