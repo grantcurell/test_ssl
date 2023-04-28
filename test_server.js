c67e899991c90b2be470b3dae1efb69b87a203ed
Grant Curell <grantcurell@gmail.com>
2023-04-28 09:02:55 -0400

    Works

--- test_server.js
+++ test_server.js
@@ -1,40 +1,53 @@
-const fs = require('fs');
-const https = require('https');
-
-// Read the CA certificate and server certificate and key from files
-const caCert = fs.readFileSync('./server_certs/rootCA.crt');
-const serverCert = fs.readFileSync('./server_certs/grant.dell.com.crt');
-const serverKey = fs.readFileSync('./server_certs/grant.dell.com.key');
-
-// Create options for the HTTPS server
-const serverOptions = {
-  ca: caCert,
-  cert: serverCert,
-  key: serverKey
+// Node.js program to demonstrate the
+// tlsSocket.getPeerCertificate() method
+ 
+const tls = require('tls'),
+    fs = require('fs'),
+ 
+    // Port and host address for server
+    PORT = 1337,
+    HOST = '127.0.0.1',
+    value = null;
+ 
+// Private key and public certificate for access
+const options = {
+    key: fs.readFileSync('./server_certs/server.key'),
+    cert: fs.readFileSync('./server_certs/server.crt'),
+    rejectUnauthorized: false
 };
-
-// Create the HTTPS server
-const server = https.createServer(serverOptions, (req, res) => {
-  res.writeHead(200);
-  res.end('Hello, world!');
+ 
+// Creating and initializing server
+const server = tls.createServer(options, function (socket) {
+ 
+    // Print the data that we received
+    socket.on('data', function (data) {
+        console.log('\nReceived: %s ',
+            data.toString().replace(/(\n)/gm, ""));
+    });
+ 
+    // Stopping the server
+    // by using the close() method
+    server.close(() => {
+        console.log("Server closed successfully");
+    });
 });
-
-// Validate client certificates on incoming requests
-server.on('secureConnection', (tlsSocket) => {
-  console.log('Client connected with certificate:')
-  const cert = tlsSocket.getPeerCertificate();
-  if (!cert || cert.issuerCertificate !== caCert) {
-    tlsSocket.destroy(new Error('Invalid client certificate'));
-  } else {
-    console.log('Valid client certificate:');
-    console.log(`Subject: ${cert.subject}`);
-    console.log(`Issuer: ${cert.issuer}`);
-    console.log(`Valid from: ${cert.valid_from}`);
-    console.log(`Valid to: ${cert.valid_to}`);
-  }
+ 
+// Start listening on a specific port and address
+// by using listen() method
+server.listen(PORT, HOST, function () {
+    console.log("I'm listening at %s, on port %s", HOST, PORT);
 });
-
-// Start the server listening on port 443 (HTTPS)
-server.listen(443, '0.0.0.0', () => {
-  console.log('Server listening on port 443');
+ 
+// Creating and initializing client
+const client = tls.connect(PORT, HOST, options, function () {
+ 
+    // Getting peer certificate
+    // by using tlsSocket.getPeerCertificate() method
+    let value = client.getPeerCertificate(false);
+ 
+    client.write("peer certificate : " + value.serialNumber);
+ 
+    client.end(() => {
+        console.log("Client closed successfully");
+    });
 });
