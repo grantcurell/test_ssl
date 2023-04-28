12ad9eee24f82110dc16b8c2abf1b072f809b842
Grant Curell <grantcurell@gmail.com>
2023-04-28 09:04:40 -0400

    works

--- test_server.js
+++ test_server.js
@@ -1,53 +1,53 @@
 // Node.js program to demonstrate the
 // tlsSocket.getPeerCertificate() method
- 
+
 const tls = require('tls'),
-    fs = require('fs'),
- 
-    // Port and host address for server
-    PORT = 1337,
-    HOST = '127.0.0.1',
-    value = null;
- 
+	fs = require('fs'),
+
+	// Port and host address for server
+	PORT = 1337,
+	HOST = '127.0.0.1';
+
 // Private key and public certificate for access
 const options = {
-    key: fs.readFileSync('./server_certs/server.key'),
-    cert: fs.readFileSync('./server_certs/server.crt'),
-    rejectUnauthorized: false
+  key: fs.readFileSync('./server_certs/server.key'),
+  cert: fs.readFileSync('./server_certs/server.crt'),
+	rejectUnauthorized: false
 };
- 
+
 // Creating and initializing server
 const server = tls.createServer(options, function (socket) {
- 
-    // Print the data that we received
-    socket.on('data', function (data) {
-        console.log('\nReceived: %s ',
-            data.toString().replace(/(\n)/gm, ""));
-    });
- 
-    // Stopping the server
-    // by using the close() method
-    server.close(() => {
-        console.log("Server closed successfully");
-    });
+
+	// Getting peer certificate
+	// by using tlsSocket.getPeerCertificate() method
+	value = socket.getPeerCertificate(true);
+	socket.write("peer certificate : " + value);
+
+	// Stopping the server
+	// by using the close() method
+	server.close(() => {
+		console.log("Server closed successfully")
+	});
 });
- 
+
 // Start listening on a specific port and address
 // by using listen() method
 server.listen(PORT, HOST, function () {
-    console.log("I'm listening at %s, on port %s", HOST, PORT);
+	console.log("I'm listening at %s, on port %s", HOST, PORT);
 });
- 
+
 // Creating and initializing client
 const client = tls.connect(PORT, HOST, options, function () {
- 
-    // Getting peer certificate
-    // by using tlsSocket.getPeerCertificate() method
-    let value = client.getPeerCertificate(false);
- 
-    client.write("peer certificate : " + value.serialNumber);
- 
-    client.end(() => {
-        console.log("Client closed successfully");
-    });
+	console.log("client is connected");
+});
+
+client.on("data", function (data) {
+
+	console.log('Received: %s',
+		data.toString().replace(/(\n)/gm, ""));
+
+	// Close the connection after receiving the message
+	client.end(() => {
+		console.log("client closed successfully")
+	});
 });
